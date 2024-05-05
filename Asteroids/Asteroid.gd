25301c43c1e49a008091c23eb69442ac9b85789e
A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>
2024-05-05 15:50:00 +0200

    Initial laser hits

--- Asteroids/Asteroid.gd
+++ Asteroids/Asteroid.gd
@@ -3,3 +3,9 @@ extends RigidBody2D
 
 func exit_map() -> void:
 	queue_free()
+
+
+func _on_detector_body_entered(body: Node2D) -> void:
+	if body.has_method(&"hit_target") and body.hit_target():
+		visible = false
+		queue_free()
--- Player/Laser/Laser.gd
+++ Player/Laser/Laser.gd
@@ -3,3 +3,8 @@ extends RigidBody2D
 
 func exit_map() -> void:
 	queue_free()
+
+
+func hit_target() -> bool:
+	queue_free()
+	return true
